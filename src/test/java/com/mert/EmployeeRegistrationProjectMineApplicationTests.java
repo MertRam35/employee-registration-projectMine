a2e5ada901937cf33b0dc2590c06944cc732bc9c
MertRam <mert.ramadan35@gmail.com>
2023-08-19 23:04:30 +0300

    Initial commit

---
+++ src/test/java/com/mert/EmployeeRegistrationProjectMineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationProjectMineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
