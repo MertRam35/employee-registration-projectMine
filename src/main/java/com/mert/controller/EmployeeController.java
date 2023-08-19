37eb96f2859dd11a89224bb685a2cf9910c9e181
MertRam <mert.ramadan35@gmail.com>
2023-08-19 23:19:14 +0300

    Employee lab started

---
+++ src/main/java/com/mert/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.mert.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    public String createEmployee(){
+
+
+        return "employee/employee-create";
+    }
+
+
+}
