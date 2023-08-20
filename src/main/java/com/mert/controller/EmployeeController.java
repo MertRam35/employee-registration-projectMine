29dde74803ce66cb608d50f0efe9d986ede7301f
MertRam <mert.ramadan35@gmail.com>
2023-08-20 09:57:22 +0300

    state list and datagenerator added

--- src/main/java/com/mert/controller/EmployeeController.java
+++ src/main/java/com/mert/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.mert.controller;
 
+import com.mert.bootstrap.DataGenerator;
+import com.mert.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employee")
 public class EmployeeController {
     @GetMapping("/register")
-    public String createEmployee() {
+    public String createEmployee(Model model) {
 
+        model.addAttribute("employee",new Employee());
+
+model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "employee/employeeCreate";
     }
